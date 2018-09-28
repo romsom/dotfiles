5eac835ca03a8d0f7063db2b994becfb06e3bae7
Roman Sommer <roman.sommer@fau.de>
2018-09-28 03:29:03 +0200

    xmonad: adjust for daw use

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -18,16 +18,24 @@ myPP = xmobarPP { ppCurrent = xmobarColor "#429942" "" . wrap "<" ">" }
 toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
 
 myConfig = def
-       { terminal   = "termite"
+       { -- terminal   = "urxvt +sb -rv"
+	 terminal = "termite --class 'termite-aux'"
        , modMask    = mod4Mask
        -- workspaces
-       , workspaces = ["1:term", "2:edit", "3:web", "4:mail"] ++ map show [5 .. 9 :: Int]
+       , workspaces = ["1:term", "2:edit", "3:qjackctl", "4:ardour", "5:setbfree", "6:guitarix"] ++ map show [7,8] ++ ["9:web"]
        , manageHook = myManageHook <+> manageHook def -- uses default to
        }
 
 myKeybindings = \c -> additionalKeysP c $
   [ ("M-S-s", spawn "i3lock && systemctl suspend")
   , ("M-S-l", spawn "i3lock -d -I5 ")
+  , ("M-S-h p", spawn "systemctl poweroff")
+  , ("M-S-h r", spawn "systemctl reboot")
+  , ("M-S-p a", spawn "ardour5")
+  , ("M-S-p p", spawn "pavucontrol")
+  , ("M-S-p c", spawn "carla")
+  , ("M-S-p q", spawn "qjackctl")
+  , ("M-S-p d", spawn "deadbeef")
   ]
 
 -- manage workspaces
@@ -38,8 +46,12 @@ myManageHook = composeAll . concat $
     , [ resource    =? r                 --> doIgnore | r <- myIgnores]
     , [ title       =? "term-main"       --> doShift "1:term" ]
     , [ title       =? "emacs-main"      --> doShift "2:edit" ]
-    , [ className   =? "Firefox"         --> doShift "3:web" ]
-    , [ className   =? "term-mutt"       --> doShift "4:mail" ]
+    , [ className   =? "qjackctl"        --> doShift "3:qjackctl" ]
+    , [ className   =? "Pavucontrol"        --> doShift "3:qjackctl" ]
+    , [ className   =? "ardour.*"         --> doShift "4:ardour" ]
+    , [ title       =? "setBfree"        --> doShift "5:setbfree" ]
+    , [ className   =? "Guitarix"        --> doShift "6:guitarix" ]
+    , [ className   =? "Firefox"         --> doShift "9:web" ]
     ]
     where
         myIgnores       = ["panel", "stalonetray", "trayer"] -- sticky, respect geometry
