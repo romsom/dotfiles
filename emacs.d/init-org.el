723aa7e43e330acdf108ee049eec87572de33832
Roman Sommer <roman.sommer@fau.de>
2021-01-18 16:07:11 +0100

    emacs: add shortcut (C-c a e) to export latex body from org

--- emacs.d/init-org.el
+++ emacs.d/init-org.el
@@ -1,4 +1,19 @@
 ;; OrgMode Options
+
+(defun my:org-latex-export-latex-body
+    (&optional async subtreep visible-only ext-plist)
+  "Export buffer to latex as bare body without preamble"
+  (interactive)
+  ;; (org-latex-export-to-latex :body-only t :async async :subtreep subtreep :visible-only visible-only :ext-plist ext-plist)))
+  (org-latex-export-to-latex async subtreep visible-only t ext-plist))
+
+(use-package org
+  :bind
+  (:map org-mode-map
+	("C-c a e" . my:org-latex-export-latex-body)))
+;; (require 'org)
+;; (define-key org-mode-map (kbd "C-c a e") 'my:org-latex-export-latex-body)
+
 ;; fontify code in code blocks a.k.a. syntax highlighting in code blocks
 (setq org-src-fontify-natively t)
 (put 'erase-buffer 'disabled nil)
@@ -8,6 +23,7 @@
 		(shell . t)
 		(emacs-lisp . t)
 		(perl . t)
+		(latex . t)
 		(R . t)))
 (setq org-adapt-indentation nil)
 (setq org-agenda-files '("~/Repo/BAi4/ba-thesis/" "~/Syncs/org/"))
