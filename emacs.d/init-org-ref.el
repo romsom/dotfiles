6d9a339e42742f6541a8d29ddc3aeb8cfb281c93
Roman Sommer <roman@resonant-bytes.de>
2021-08-14 14:21:06 +0200

    emacs: configure org-ref

--- emacs.d/init-org-ref.el
+++ emacs.d/init-org-ref.el
@@ -2,10 +2,19 @@
   :ensure t
   :after helm
   ;; TODO add global bibliography if needed
-  ;;:custom
+  :custom
+  ;; Config from README
+  (reftex-default-bibliography '("~/Repo/BAi4/ba-thesis/bibliography.bib"))
+  (org-ref-default-bibliography '("~/Repo/BAi4/ba-thesis/bibliography.bib"))
+  (org-ref-pdf-directory "~/Repo/BAi4/ba-thesis/literature/")
+  (org-ref-bibliography-notes "~/Repo/BAi4/ba-thesis/references.org")
+  ;; Config for helm
+  (bibtex-completion-bibliography "~/Repo/BAi4/ba-thesis/bibliography.bib")
   ;; (bibtex-completion-bibliography "~/Sync/bibliography/references.bib")
-  ;; (bibtex-completion-library-path "~/Sync/bibliography/bibtex-pdfs")
-  ;; (bibtex-completion-notes-path "~/Sync/bibliography/helm-bibtex-notes")
+  (bibtex-completion-library-path "~/~/Repo/BAi4/ba-thesis/literature/")
+  (bibtex-completion-notes-path "~/Repo/BAi4/ba-thesis/references.org")
+  :config
+  (setq org-ref-default-ref-type "cref")
   )
 (use-package doi-utils
   :after org-ref)
